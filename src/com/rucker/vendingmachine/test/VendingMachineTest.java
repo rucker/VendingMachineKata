c6818d11de6e4b13abbb668be50bc88ad1e2a5b8
rucker <rick.ucker@gmail.com>
2015-03-16 18:48:31 -0400

    Refactor passing tests.

--- src/com/rucker/vendingmachine/test/VendingMachineTest.java
+++ src/com/rucker/vendingmachine/test/VendingMachineTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.rucker.vendingmachine.VendingMachine;
@@ -12,16 +13,21 @@ import com.rucker.vendingmachine.components.Display;
 import com.rucker.vendingmachine.currency.Coin;
 
 public class VendingMachineTest {
+	
+	private VendingMachine vendingMachine;
+	
+	@Before
+	public void setUp() {
+		vendingMachine = new VendingMachine();
+	}
 
 	@Test
 	public void whenNoCoinsHaveBeenInsertedDisplayReadsInsertCoin() {
-		VendingMachine vendingMachine = new VendingMachine();
 		assertEquals(Display.INSERT_COIN, vendingMachine.getDisplayMessage());
 	}
 
 	@Test
 	public void whenNoCoinsHaveBeenInsertedTotalMoneyReceivedIsZero() {
-		VendingMachine vendingMachine = new VendingMachine();
 		vendingMachine.receiveCoin(Coin.NICKEL.weight, Coin.NICKEL.diameter, Coin.NICKEL.diameter);
 		assertEquals(new BigDecimal(0).setScale(2, RoundingMode.HALF_UP), vendingMachine.getTotalMoneyReceived());
 	}
