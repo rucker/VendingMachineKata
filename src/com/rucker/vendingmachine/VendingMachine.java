5ae6839cb9607ee98eb68f425ba70325803dfa4f
rucker <rick.ucker@gmail.com>
2015-03-16 18:47:07 -0400

    Add test case: total money rec'd is zero before inserting coins.

--- src/com/rucker/vendingmachine/VendingMachine.java
+++ src/com/rucker/vendingmachine/VendingMachine.java
@@ -1,12 +1,32 @@
 package com.rucker.vendingmachine;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import com.rucker.vendingmachine.components.Display;
 
 public class VendingMachine {
 	
 	private Display display = new Display();
+	private BigDecimal totalMoneyReceived;
+
+	public VendingMachine() {
+		setTotalMoneyReceivedToZero();
+	}
+	private void setTotalMoneyReceivedToZero() {
+		totalMoneyReceived = new BigDecimal(0).setScale(2, RoundingMode.HALF_UP);
+	}
 	
 	public String getDisplayMessage() {
 		return display.getMessage();
+	}
+
+	public void receiveCoin(double weight, double diameter, double thickness) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public BigDecimal getTotalMoneyReceived() {
+		return totalMoneyReceived;
 	}
 }
--- src/com/rucker/vendingmachine/test/VendingMachineTest.java
+++ src/com/rucker/vendingmachine/test/VendingMachineTest.java
@@ -1,11 +1,15 @@
 package com.rucker.vendingmachine.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.junit.Test;
 
 import com.rucker.vendingmachine.VendingMachine;
 import com.rucker.vendingmachine.components.Display;
+import com.rucker.vendingmachine.currency.Coin;
 
 public class VendingMachineTest {
 
@@ -15,4 +19,10 @@ public class VendingMachineTest {
 		assertEquals(Display.INSERT_COIN, vendingMachine.getDisplayMessage());
 	}
 
+	@Test
+	public void whenNoCoinsHaveBeenInsertedTotalMoneyReceivedIsZero() {
+		VendingMachine vendingMachine = new VendingMachine();
+		vendingMachine.receiveCoin(Coin.NICKEL.weight, Coin.NICKEL.diameter, Coin.NICKEL.diameter);
+		assertEquals(new BigDecimal(0).setScale(2, RoundingMode.HALF_UP), vendingMachine.getTotalMoneyReceived());
+	}
 }
