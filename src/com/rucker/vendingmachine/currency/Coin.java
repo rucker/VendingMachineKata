52a7db5e524a8003ce1f39bdfd3ddc40cf85c5df
rucker <rick.ucker@gmail.com>
2015-03-16 18:19:27 -0400

    Update Coin definition to include coins' values.

--- src/com/rucker/vendingmachine/currency/Coin.java
+++ src/com/rucker/vendingmachine/currency/Coin.java
@@ -1,10 +1,12 @@
 package com.rucker.vendingmachine.currency;
 
+import java.math.BigDecimal;
+
 public enum Coin {
-	NICKEL(5.0, 21.21, 1.95),
-	DIME(2.268, 17.91, 1.35),
-	QUARTER(5.670, 24.26, 1.75),
-	METAL_SLUG(-1, -1, -1);
+	NICKEL(5.0, 21.21, 1.95, new BigDecimal(.05)),
+	DIME(2.268, 17.91, 1.35, new BigDecimal(.10)),
+	QUARTER(5.670, 24.26, 1.75, new BigDecimal(.25)),
+	METAL_SLUG(-1, -1, -1, new BigDecimal(.00));
 	
 	//Measured in grams.
 	public final double weight;
@@ -13,7 +15,7 @@ public enum Coin {
 	//Measured in millimeters.
 	public final double thickness;
 
-	Coin(double weight, double diameter, double thickness) {
+	Coin(double weight, double diameter, double thickness, BigDecimal value) {
 		this.weight = weight;
 		this.diameter = diameter;
 		this.thickness = thickness;
